2e9551fb2235e944dc77edd9ece888fb50532a04
Roberto Rodriguez <rrodriguez@incompanysolutions.com>
2011-04-11 10:19:17 -0500

    added rest js

--- init.rb
+++ init.rb
@@ -3,6 +3,8 @@ require 'sinatra'
 require 'haml'
 require 'ninesixty'
 require 'json'
+require 'excelsior'
+require 'rforce'
 
 get '/' do
  haml :index,{:layout => :homeLayout}
@@ -26,6 +28,52 @@ end
 
 get '/contacto' do
  haml :contacto
+end
+
+get '/importCSV' do
+  content_type 'text/html', :charset => 'utf-8'
+    ENV['SHOWSOAP'] = 'true'
+
+      binding = RForce::Binding.new \
+         'https://www.salesforce.com/services/Soap/u/20.0'
+
+       binding.login \
+         'salesforce@rodcocr.com', 'company1'
+   
+   rows = []
+   
+   
+    Excelsior::Reader.rows(File.open('/Users/roberto/Proyectos/Rodco/svn/ruby/web/public/productos.csv', 'rb')) do |row|
+     rows << row
+   end
+
+
+    productos=[]
+    rows.each do |producto|
+      producto = {:type,'Producto__c',:CodigoExterno__c,    producto[0], :NombreWeb__c,      producto[1] , 'descripcion__c' , producto[2] }
+    
+      productos.push(producto)
+    end
+      
+      
+        while productos.length > 0
+          upsert = [];
+          upsert.push(:externalIDFieldName,'CodigoExterno__c');
+          while upsert.length < 390  && productos.length > 0
+            upsert.push(:sObjects)
+            upsert.push(productos.pop)
+          end
+          binding.upsert upsert
+        end
+        
+        
+      
+     
+           
+   
+ 
+
+ 
 end
 
 def render_file(filename)
--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,22 @@
+ <div id="fb-root"></div>
+
  
+function loadFacebook(){
+	
+	 FB.init({ apiKey: '6c5b74defb24efdead3232f0db0b3c3d' });
+	
+	FB.api('/165218380197907/feed', function(response) {
+	 // alert(response.name);
+	});
+	
+
+}
 
 $(document).ready(function(){
-  
+	loadFacebook();
+
+
+
 });
 
  
